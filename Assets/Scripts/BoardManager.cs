57b80a9ba555ad90ddbcc6328bed140f3b9a0807
Andres Mrad (Q-ro) <andres.mrad@gmail.com>
2015-08-24 22:54:52 -0500

    Board updates
    Added scrtipt to handdle the "board" oh the game, updates sprite sheet
    with "original art"

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count (int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    //Amount of rows and columns for the game board
+    public int column = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

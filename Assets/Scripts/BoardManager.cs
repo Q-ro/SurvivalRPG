c4c685f26b063c7e094376e0edf8d47e93dcbf27
Andres Mrad (Q-ro) <andres.mrad@gmail.com>
2015-08-25 20:14:49 -0500

    Grid update
    Updated tile sets, updated grid manager script to better manage game
    objects to be ramdomly placed inside caves and such

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System.Collections;
 using Random = UnityEngine.Random;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour {
 
@@ -21,8 +22,36 @@ public class BoardManager : MonoBehaviour {
     //Amount of rows and columns for the game board
     public int column = 8;
     public int rows = 8;
+
+    //Range of obstacles to spawn
     public Count wallCount = new Count(5, 9);
+    //Range of items to spwan
     public Count foodCount = new Count(1, 5);
+
+    //Game objects to be used
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    //A parent for our game objects, for cleanliness sake
+    private Transform boardHolder;
+
+    //A gird of all the vailable positions to spawn object into
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    /// <summary>
+    /// Function that initialize the grid for our game
+    /// </summary>
+    void InitializeList()
+    {
+        //Make sure the grid is empty
+        gridPositions.Clear();
+
+
+    }
 
 	// Use this for initialization
 	void Start () {
